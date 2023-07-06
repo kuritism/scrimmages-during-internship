ca8ae12d0d9e354bb569b99ca49164ab1d750c44
Kay <108748103+kaylascout24@users.noreply.github.com>
2023-07-06 11:43:20 -0400

    BETTER JUMP

--- battle.py
+++ battle.py
@@ -1,10 +1,7 @@
-import cv2
-import pygame, sys, time, random
-from pygame import mixer
-from tinytag import TinyTag
+import pygame, sys, time
 
 pygame.init()
-mixer.init()
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -36,26 +33,35 @@ class players(pygame.sprite.Sprite):
         """Initialize the player"""
         super().__init__()
         self.DEFAULT_IMAGE_SIZE = (160, 160)
-        self.image = pygame.transform.scale(pygame.image.load("scrimmaging/characters/bingo/sprites/bing_Face_Right.png"), self.DEFAULT_IMAGE_SIZE)
+        self.rightimage = pygame.transform.scale(pygame.image.load("scrimmaging/characters/bingo/sprites/bing_Face_Right.png"), self.DEFAULT_IMAGE_SIZE)
+        self.leftimage = pygame.transform.scale(pygame.image.load("scrimmaging/characters/bingo/sprites/bingo_Face_Left.png"), self.DEFAULT_IMAGE_SIZE)
+        self.image = self.rightimage
         self.rect = self.image.get_rect()
         self.rect.centerx = WINDOW_WIDTH / 2
         self.rect.bottom = WINDOW_HEIGHT
         self.yvelocity = 8
-        self.xvelocity = 6
+        self.xvelocity = 0
         self.m = 1
         self.is_jump = False
+        self.crouching = False
+        self.crouch_height = WINDOW_HEIGHT + 90
 
     def update(self):
         """Update the player"""
         keys = pygame.key.get_pressed()
+        self.xvelocity = 0
 
         # Move the player within the bounds of the screen
         if keys[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= self.xvelocity
+            self.xvelocity = -6
+            self.image = self.leftimage
         if keys[pygame.K_RIGHT] and self.rect.right < WINDOW_WIDTH:
-            self.rect.x += self.xvelocity
+            self.xvelocity = 6
+            self.image = self.rightimage
         if keys[pygame.K_UP] and self.rect.bottom == WINDOW_HEIGHT:
             self.is_jump = True
+        if keys[pygame.K_DOWN] and not self.is_jump:
+            self.crouching = True
 
         if self.is_jump:
             F = (1 / 2) * self.m * (self.yvelocity ** 2)
@@ -64,14 +70,17 @@ class players(pygame.sprite.Sprite):
             print(self.yvelocity)
             if self.yvelocity < 0:
                 self.m = -1
-            if self.rect.bottom >= WINDOW_HEIGHT:
-                self.is_jump = False
-                self.yvelocity = 8
-                self.rect.bottom = WINDOW_HEIGHT
-                self.m = 1
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.is_jump = False
+            self.yvelocity = 8
+            self.rect.bottom = WINDOW_HEIGHT
+            self.m = 1
 
-        pygame.time.delay(10)
-
+        if self.crouching:
+            self.rect.bottom = self.crouch_height
+            self.xvelocity = 0
+            self.crouching = False
+        self.rect.x += self.xvelocity
 
 class arena():
     pass
@@ -82,38 +91,15 @@ my_player_group = pygame.sprite.Group()
 my_player = players()
 my_player_group.add(my_player)
 
-# Pick background
-bg = cv2.VideoCapture("Assets/Backgrounds/" + str(random.randint(1, 1)) + ".mp4")
-success, bg_image = bg.read()
-bg_fps = bg.get(cv2.CAP_PROP_FPS)
-bg_clock = pygame.time.Clock()
-
-# Pick music
-bgm = "audio/BGM/1.mp3"
-mixer.music.load(bgm)
-mixer.music.set_volume(1)
-mixer.music.play(-1)
-song_name = TinyTag.get(bgm)
-print("Title: " + song_name.title)
-
 # Main game loop
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Play Music
-
-    # Play Background
-    bg_clock.tick(bg_fps)
-    success, bg_image = bg.read()
-
-    bg_surf = pygame.image.frombuffer(bg_image.tobytes(), bg_image.shape[1::-1], "BGR")
-
     # Blit background
-    display_surface.blit(bg_surf, (160, 0))
+    display_surface.fill((20, 175, 175))
 
     # Blit text
 
